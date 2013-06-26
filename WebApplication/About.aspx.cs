4bd8967591111b152f1222cb29ce0986994fb61a
unknown <stephen@logentries.com>
2013-06-26 10:49:37 +0100

    initial project

---
+++ WebApplication/About.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using log4net;
+namespace WebApplication
+{
+    public partial class About : Page
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(About));
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            logger.Debug("test");
+        }
+    }
+}
